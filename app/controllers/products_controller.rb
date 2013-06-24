29b5c807d0e65425ad0789acda08b354bbd997db
Dzianis Dashkevich <dskecse@gmail.com>
2013-06-24 15:57:34 +0300

    make products_controller skinnier

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,18 @@
 class ProductsController < ApplicationController
+  before_filter :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
     respond_with(@products)
   end
 
   def show
-    @product = Product.find(params[:id])
     respond_with(@product)
   end
 
   def new
     @product = Product.new
     respond_with(@product)
-  end
-
-  def edit
-    @product = Product.find(params[:id])
   end
 
   def create
@@ -25,14 +22,18 @@ class ProductsController < ApplicationController
   end
 
   def update
-    @product = Product.find(params[:id])
     @product.update_attributes(params[:product])
     respond_with(@product)
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     respond_with(@product)
   end
+
+  protected
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 end
